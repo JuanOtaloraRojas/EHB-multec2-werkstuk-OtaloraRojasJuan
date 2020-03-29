3dc75fc4430ff23cf0b1025d0312887c5d77882b
Otalora_Rojas_Juan_Carlos <juan.otalora.rojas@student.ehb.be>
2020-03-29 16:48:19 +0200

    begin van code, see more / less

--- index.js
+++ index.js
@@ -8,23 +8,50 @@ async function getMovies(){
 
     //log data from response into json
     const data = await response.json();
-    console.log('Data: ', data);
+    console.log('Data: ', data);     
 
+
+    //Array sort of recieved data
+    const comparedArray = data.sort((a, b) => (a.title > b.title) ? 1 : (a.title === b.title) ? ((a.title > b.title) ? 1 : -1) : -1 )
+    console.log(comparedArray);
+    console.log("Compared:", comparedArray);
+
+
+    //Make array to give each new id
+    var myArray = [];        
+    for (var i = 0; i < data.length; i++){
+      myArray[i] += 1;
+  }
+
+  console.log(myArray);
+
+
+    //Filter based on top rated scores
+    //function filter() {
+        //const scoreFilter = data.sort((a, b) => (a.rt_score > b.rt_score) ? 1 : (a.rt_score === b.rt_score) ? ((a.rt_score > b.rt_score) ? 1 : -1) : -1 )
+       // console.log(scoreFilter);
+      //}
+    
 
     //get specific data from every array (forEach)
     let htmlString ='';
     data.forEach(movies => {htmlString += 
         `<ul>
             <h2> ${movies.title} </h2>
-             <li> ${movies.id} </li> 
-             <li> ${movies.title} </li>
-             <li> ${movies.description} </li> 
-             <li> ${movies.director} </li>
-             <li> ${movies.producer} </li> 
-             <li> ${movies.release_date} </li>
+            <p></p>
+            <span id="dots">...</span>
+            <span id="more">
+             <li id="Description"[myArray]"> Description: ${movies.description} </li> 
+             <li id="Director[myArray]"> Director: ${movies.director} </li>
+             <li id="Producer[myArray]"> Producer: ${movies.producer} </li> 
+             <li id="Realease Date[myArray]"> Release Date: ${movies.release_date} </li>
+             </span>
+             <p></p>
+             <button onclick= "SeeMore()" id = SeeMore>See more</button>        
          </ul>`
     });
-
+ 
+     
     //log selected data from array
     console.log('htmlString', htmlString);
 
@@ -32,24 +59,5 @@ async function getMovies(){
     let moviesList = document.getElementById('allMovies');
     moviesList.innerHTML = htmlString;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 getMovies();
